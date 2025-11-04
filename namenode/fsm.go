6c97d6b36b93c78afdcc4d14b6607a9ea345c152
Rahul6700 <rahulsk4087@gmail.com>
2025-11-04 13:06:05 +0530

    progress

--- namenode/fsm.go
+++ namenode/fsm.go
@@ -2,9 +2,11 @@ package namenode
 
 import (
 	"encoding/json"
+	"bytes"
 	"io"
 	"foodo/shared"
 	"food/model"
+	"log"
 )
 
 // we return a pointer to the FSM, so that whereever its modified from, we always acess the same FMS
@@ -19,3 +21,21 @@ func NewFsm() *FSM {
 }
 
 // to implement : apply, snapshot and restore
+
+// Apply function, 
+func (the_fsm *FSM) Apply (log *raft.log) interface{} {
+	the_fsm.lock.Lock()
+	defer the_fsm.lock.Unlock()
+
+	var cmd shared.RaftCommand
+	if err := json.Unmarshal(log.Data, &cmd); err != nil {
+		return log.Printf("could not unmarshal command: %s\n", err)
+	}
+
+	if(cmd.Operation != "REGISTER_FILE") return log.Printf("unknown operation %s\n", cmd.Operation)
+	else {
+		var chunkIDSlice []string
+		for _, chunk := range cmd.Chunks {
+		}
+	}
+}
